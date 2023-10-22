9506d72e7de0bfcc27816256ef6c20d872b4e3ff
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-10-22 22:39:10 +0200

    feat: add application functionalty
    Create the Hero class, the atacar() method and do the logic

--- 03-Hero-Class/index.js
+++ 03-Hero-Class/index.js
@@ -1,3 +1,32 @@
 class Hero {
-  constructor() {}
+  constructor(nome, idade, tipoDeHeroi) {
+    this.name = nome;
+    this.idade = idade;
+    this.tipoDeHeroi = tipoDeHeroi;
+  }
+
+  atacar() {
+    let ataque;
+    switch (this.tipoDeHeroi) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "ataque não definido";
+        break;
+    }
+    console.log(`O ${this.tipoDeHeroi} atacou usando ${ataque}`);
+  }
 }
+
+let heroi = new Hero("Eric", 25, "guerreiro");
+heroi.atacar();
